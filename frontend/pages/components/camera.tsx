b86182f7065ef02fe32ad4b305e0d8236d54d134
Haruki <tench8iphy@icloud.com>
2023-01-26 12:09:02 +0900

    カメラ機能改修1

--- frontend/pages/components/camera.tsx
+++ frontend/pages/components/camera.tsx
@@ -1,18 +1,22 @@
 import React, { useState, useRef } from "react";
 import { Camera } from "react-camera-pro";
+import styles from "../../styles/Home.module.css";
 
-export default function () {
+export default function TakePhoto() {
   const camera: any = useRef(null);
   const [image, setImage] = useState(null);
 
   return (
     <div>
-      <Camera ref={camera} errorMessages={{
-        noCameraAccessible: undefined,
-        permissionDenied: undefined,
-        switchCamera: undefined,
-        canvas: undefined
-      }} />
+      <div className={styles.camera}>
+        <Camera ref={camera} errorMessages={{
+          noCameraAccessible: undefined,
+          permissionDenied: undefined,
+          switchCamera: undefined,
+          canvas: undefined
+        }} />
+      </div>
+
       <button onClick={() => setImage(camera.current.takePhoto())}>Take photo</button>
       <img src={image} alt="TakePhoto" />
     </div>
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import { House, PencilSimpleLine, Books, ChartPieSlice } from 'phosphor-react'
-import Component from './components/camera'
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -10,11 +10,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div>
+        <p>あいうえお</p>
+      </div>
 
       <footer className={styles.footer}>
         <House size={32} />
         <PencilSimpleLine size={32} />
-        <Books size={32} />
+        <Link href="/Words"><Books size={32} /></Link>
         <ChartPieSlice size={32} />
       </footer>
     </div>
