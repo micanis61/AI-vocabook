ac0dc9fcd3b939df52c8f62b96cde55d64bb6135
Haruki <tench8iphy@icloud.com>
2023-02-02 11:10:53 +0900

    TEST

---
+++ test/vision-api_test/ocr-test1.py
@@ -0,0 +1,31 @@
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    #いらんねprint('Texts:')
+    print(texts)
+
+    # for text in texts:
+    #     print('\n"{}"'.format(text.description))
+
+    #     vertices = (['({},{})'.format(vertex.x, vertex.y)
+    #                 for vertex in text.bounding_poly.vertices])
+
+    #     print('bounds: {}'.format(','.join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+detect_text('resources/kinnnohure-zu.jpg')
--- test/vision-api_test/test.py
+++ test/vision-api_test/test.py
@@ -1,6 +1,6 @@
 import io
 import os
-import deepl
+#import deepl
 
 # Imports the Google Cloud client library
 from google.cloud import vision
@@ -9,7 +9,7 @@ from google.cloud import vision
 client = vision.ImageAnnotatorClient()
 
 # The name of the image file to annotate
-file_name = ('resources/wagahai.jpg')
+file_name = ('resources/kinnnohure-zu.jpg')
 
 # Loads the image into memory
 with io.open(file_name, 'rb') as image_file:
@@ -25,12 +25,12 @@ text = ""
 
 for t in texts:
         #print('\n"{}"'.format(text.description))
-        
+
         vertices = (['({},{})'.format(vertex.x, vertex.y)
                     for vertex in t.bounding_poly.vertices])
 
         #print('bounds: {}'.format(','.join(vertices)))
-        
+
         text = text + t.description
 
 
@@ -38,31 +38,31 @@ for t in texts:
 print(text)
 
 
-if response.error.message:
-        raise Exception(
-            '{}\nFor more info on error messages, check: '
-            'https://cloud.google.com/apis/design/errors'.format(
-                response.error.message))
+# if response.error.message:
+#         raise Exception(
+#             '{}\nFor more info on error messages, check: '
+#             'https://cloud.google.com/apis/design/errors'.format(
+#                 response.error.message))
 
 
-###########################################################################
-API_KEY = '2905e011-e25a-9832-fc1b-7f65f1be1d8a:fx' # 自身の API キーを指定
+# ###########################################################################
+# API_KEY = '2905e011-e25a-9832-fc1b-7f65f1be1d8a:fx' # 自身の API キーを指定
 
-#text = 'Riemann Zeta function is a very important function in number theory.'
-source_lang = 'JA'
-target_lang = 'EN-US'
+# #text = 'Riemann Zeta function is a very important function in number theory.'
+# source_lang = 'JA'
+# target_lang = 'EN-US'
 
-# イニシャライズ
-translator = deepl.Translator(API_KEY)
-
-
-# 翻訳を実行
-
-result = translator.translate_text(text, source_lang=source_lang, target_lang=target_lang)
-
-# print すると翻訳後の文章が出力される
-#print(result)
+# # イニシャライズ
+# translator = deepl.Translator(API_KEY)
 
 
+# # 翻訳を実行
 
-print(result)
+# result = translator.translate_text(text, source_lang=source_lang, target_lang=target_lang)
+
+# # print すると翻訳後の文章が出力される
+# #print(result)
+
+
+
+# print(result)
